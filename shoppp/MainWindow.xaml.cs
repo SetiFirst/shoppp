fb74cb2a57d9899f834bd31ff88ef7e400dd5185
Александр Возженников <vozzhennikoffa@yandex.ru>
2025-04-03 21:23:59 +0300

    Обновил, добавил WrapPanel, сменил имя папки

--- shoppp/MainWindow.xaml.cs
+++ shoppp/MainWindow.xaml.cs
@@ -1,25 +1,10 @@
-﻿using shoppp.classes;
-using System;
+﻿using shoppp.model;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace shoppp
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -35,14 +20,16 @@ namespace shoppp
             if (!string.IsNullOrEmpty(searchQuery))
             {
                 ProductCollection _products = new ProductCollection();
-                List<Product> filteredCollection = new List<Product>();
-                foreach (Product product in _products)
-                {
-                    if (product.Title.ToLower().Contains(searchQuery) || product.Description.ToLower().Contains(searchQuery) || product.Category.ToLower().Contains(searchQuery) || product.Price.ToLower().Contains(searchQuery))
-                    {
-                        filteredCollection.Add(product);
-                    }
-                }
+                searchQuery = searchQuery?.ToLower() ?? ""; 
+
+                List<Product> filteredCollection = _products
+                    .Where(p =>
+                        (p.Title?.ToLower() ?? "").Contains(searchQuery) ||
+                        (p.Description?.ToLower() ?? "").Contains(searchQuery) ||
+                        (p.Category?.ToLower() ?? "").Contains(searchQuery) ||
+                        (p.Price?.ToLower() ?? "").Contains(searchQuery))
+                    .ToList();
+
                 shop.ItemsSource = filteredCollection;
             }
             else
--- shoppp/Models/Product.cs
+++ shoppp/Models/Product.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace shoppp.classes
+namespace shoppp.model
 {
     class Product
     {
